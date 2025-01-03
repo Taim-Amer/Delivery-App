a2dae6aabf5aa0e96a7943728a4661f09d5f7e84
Taim-Amer <taim.amer.000@gmail.com>
2025-01-03 01:25:28 -0800

    Apis Url

--- lib/features/authentication/views/signup/widgets/register_form.dart
+++ lib/features/authentication/views/signup/widgets/register_form.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:delivery_app/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:delivery_app/features/authentication/views/signin/login_screen.dart';
 import 'package:delivery_app/utils/constants/colors.dart';
@@ -9,96 +8,103 @@ import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:delivery_app/utils/constants/sizes.dart';
 import 'package:delivery_app/utils/validators/validation.dart';
-import 'package:delivery_app/utils/services/image_services.dart'; // استيراد الكلاس
+import 'package:delivery_app/utils/services/image_services.dart';
 
 class RegisterForm extends StatelessWidget {
   const RegisterForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return SingleChildScrollView(
       child: Form(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ValueListenableBuilder<List<File>>(
-              valueListenable: TImageServices.selectedImages,
-              builder: (context, images, child) {
-                return GestureDetector(
-                  onTap: () => TImageServices.pickSingleImage(),
-                  child: TRoundedContainer(
-                    radius: 50,
-                    showBorder: true,
-                    padding: const EdgeInsets.all(40),
-                    // backgroundColor: TColors.grey,
-                    // backgroundImage: images.isNotEmpty ? FileImage(images.first) : null,
-                    child: images.isEmpty ? const Icon(Iconsax.camera, size: 50, color: Colors.grey) : null,
-                  ),
-                );
-              },
-            ),
-            const SizedBox(height: TSizes.spaceBtwInputField),
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    cursorColor: TColors.primary,
-                    validator: (value) =>
-                        TValidator.validateEmptyText("First Name", value),
-                    expands: false,
-                    decoration: const InputDecoration(
-                        hintText: "First Name", prefixIcon: Icon(Iconsax.user)),
-                  ),
-                ),
-                const SizedBox(
-                  width: TSizes.spaceBtwInputField,
-                ),
-                Expanded(
-                  child: TextFormField(
-                    validator: (value) =>
-                        TValidator.validateEmptyText("Last Name", value),
-                    expands: false,
-                    decoration: const InputDecoration(
-                        hintText: "Last Name", prefixIcon: Icon(Iconsax.user)),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: TSizes.spaceBtwInputField),
-            TextFormField(
-              validator: (value) => TValidator.validatePhoneNumber(value),
-              expands: false,
-              decoration: const InputDecoration(
-                  hintText: "Phone Number", prefixIcon: Icon(Iconsax.call)),
-            ),
-            const SizedBox(height: TSizes.spaceBtwInputField),
-            TextFormField(
-              validator: (value) => TValidator.validatePassword(value),
-              obscureText: true,
-              decoration: const InputDecoration(
-                hintText: "Password",
-                prefixIcon: Icon(Iconsax.password_check),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ValueListenableBuilder<List<File>>(
+                valueListenable: TImageServices.selectedImages,
+                builder: (context, images, child) {
+                  return GestureDetector(
+                    onTap: () => TImageServices.pickSingleImage(),
+                    child: TRoundedContainer(
+                      radius: 30,
+                      width: 130,
+                      height: 130,
+                      showBorder: true,
+                      child: images.isNotEmpty ? ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image.file(
+                          images.first,
+                          fit: BoxFit.cover,
+                        ),
+                      ) : const Icon(Iconsax.camera, size: 50, color: Colors.grey),
+                    ),
+                  );
+                },
               ),
-            ),
-            const SizedBox(height: TSizes.spaceBtwInputField),
-            SizedBox(
-              width: double.infinity,
-              height: 50.h,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text("Create Account"),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      cursorColor: TColors.primary,
+                      validator: (value) => TValidator.validateEmptyText("First Name", value),
+                      expands: false,
+                      decoration: const InputDecoration(
+                          hintText: "First Name", prefixIcon: Icon(Iconsax.user)),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: TSizes.spaceBtwInputField,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      validator: (value) =>
+                          TValidator.validateEmptyText("Last Name", value),
+                      expands: false,
+                      decoration: const InputDecoration(
+                          hintText: "Last Name", prefixIcon: Icon(Iconsax.user)),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: TSizes.spaceBtwInputField),
-            SizedBox(
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () => Get.offAll(const SigninScreen(),
-                    transition: Transition.rightToLeft),
-                child: const Text("Already have an account"),
+              const SizedBox(height: TSizes.spaceBtwInputField),
+              TextFormField(
+                validator: (value) => TValidator.validatePhoneNumber(value),
+                expands: false,
+                decoration: const InputDecoration(
+                    hintText: "Phone Number", prefixIcon: Icon(Iconsax.call)),
               ),
-            ),
-          ],
+              const SizedBox(height: TSizes.spaceBtwInputField),
+              TextFormField(
+                validator: (value) => TValidator.validatePassword(value),
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: Icon(Iconsax.password_check),
+                ),
+              ),
+              const SizedBox(height: TSizes.spaceBtwInputField),
+              SizedBox(
+                width: double.infinity,
+                height: 50.h,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Create Account"),
+                ),
+              ),
+              const SizedBox(height: TSizes.spaceBtwInputField),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () => Get.offAll(const SigninScreen(),
+                      transition: Transition.rightToLeft),
+                  child: const Text("Already have an account"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -2,27 +2,24 @@ class TApiConstants {
 
   static const String baseUrl = 'https://pharmacy.technoplus.dev/api/';
 
-  static const String register = 'client/register';
+  static const String register = 'register';
 
-  static const String verify = 'otp/verify';
+  static const String login = 'login';
 
-  static const String resend = 'otp/resend';
+  static const String getOrders = 'orders';
 
-  static const String forget = 'ForgetPassword';
+  static const String getOrderDetails = 'orders';
 
-  static const String forgetVerify = 'otp/password/verify';
+  static const String removeOrder = 'orders';
 
-  static const String forgetReset = 'resetPassword';
+  static const String getStores = 'stores';
 
-  static const String signin = 'login';
+  static const String getStoreDetails = 'stores';
 
-  static const String create = 'orders';
+  static const String getStoreProducts = 'get_products';
 
-  static const String showOrder = 'order/show';
+  static const String getProducts = 'products';
 
-  static const String showMyOrders = 'orders';
+  static const String getProductDetails = 'products';
 
-  static const String orderStatus = 'order/progress';
-
-  static const String cancel = 'order/cancel';
 }
