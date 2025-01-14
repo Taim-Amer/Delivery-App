6978b82326695c95c98976e44380cf7a6d9b2c87
Taim-Amer <taim.amer.000@gmail.com>
2025-01-14 04:22:02 -0800

    Store search

--- lib/features/shop/controllers/store_controller.dart
+++ lib/features/shop/controllers/store_controller.dart
@@ -2,10 +2,12 @@ import 'package:delivery_app/common/widgets/alerts/snackbar.dart';
 import 'package:delivery_app/features/shop/models/store_details_model.dart';
 import 'package:delivery_app/features/shop/models/store_model.dart';
 import 'package:delivery_app/features/shop/models/store_products_model.dart';
+import 'package:delivery_app/features/shop/models/store_search_model.dart';
 import 'package:delivery_app/features/shop/repositories/store/store_repo_impl.dart';
 import 'package:delivery_app/utils/constants/enums.dart';
 import 'package:delivery_app/utils/helpers/helper_functions.dart';
 import 'package:delivery_app/utils/logging/logger.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class StoreController extends GetxController{
@@ -14,10 +16,14 @@ class StoreController extends GetxController{
   Rx<RequestState> getStoresApiStatus = RequestState.begin.obs;
   Rx<RequestState> getStoreDetailsApiStatus = RequestState.begin.obs;
   Rx<RequestState> getStoreProductsApiStatus = RequestState.begin.obs;
+  Rx<RequestState> storeSearchApiStatus = RequestState.begin.obs;
 
   final allStoresModel = StoreModel().obs;
   final storeDetailsModel = StoreDetailsModel().obs;
   final storeProductsModel = StoreProductsModel().obs;
+  final searchStoreModel = StoreSearchModel().obs;
+
+  final searchStoreController = TextEditingController();
 
   @override
   void onReady() {
@@ -56,6 +62,19 @@ class StoreController extends GetxController{
       TLoggerHelper.error(error.toString());
       THelperFunctions.updateApiStatus(target: getStoreProductsApiStatus, value: RequestState.error);
       // showSnackBar("An error occurred, please try again", AlertState.error);
+    });
+  }
+
+  Future<void> searchStore() async{
+    THelperFunctions.updateApiStatus(target: storeSearchApiStatus, value: RequestState.loading);
+    await StoreRepoImpl.instance.searchStore(
+      storeName: searchStoreController.text.toString(),
+    ).then((response){
+      searchStoreModel.value = response;
+      THelperFunctions.updateApiStatus(target: storeSearchApiStatus, value: RequestState.success);
+    }).catchError((error){
+      THelperFunctions.updateApiStatus(target: storeSearchApiStatus, value: RequestState.error);
+      showSnackBar("An error occurred or store not found, please try again", AlertState.warning);
     });
   }
 }
